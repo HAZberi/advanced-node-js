6d01f2ae66f5568be85fa24940e6707a46e2d11f
Hassaan Zuberi <mhazuberi@gmail.com>
2021-06-10 18:44:06 -0600

    building and chaining promises to call dogs api

--- async-await/index.js
+++ async-await/index.js
@@ -1,21 +1,43 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  if (err) return console.log("File Not Found");
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile(`${__dirname}/dog-image.txt`, res.body.message, (err) => {
-        if (err) return console.log("Cannot write to the file");
-        console.log("Successfully written to the file.");
-      });
-    })
-    .catch((err) => {
-      if (err) return console.log("Cannot find the requested breed");
+const readFileWithPromise = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("Cannot find the requested File");
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFileWithPromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("Unable to write the file");
+      resolve("File Written");
+    });
+  });
+};
+
+//Always remember to return a promise, so it can be chained using 
+// the ".then()" method.
+
+
+readFileWithPromise(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFileWithPromise(
+      `${__dirname}/dog-image.txt`,
+      res.body.message
+    ).then(() => {
+      console.log("Successfully written to the file.");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
